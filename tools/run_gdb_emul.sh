8d9181991b11bc76e690a8891d014fb9374de456
Vasek Sraier <git@vakabus.cz>
2018-07-13 19:16:13 +0200

    added emulator support with arm_now, added trace for sh

---
+++ tools/run_gdb_emul.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+COMMAND="./busybox-mips sh -c './busybox-mips cwd'"
+
+start_emul() {
+    cd ../mips_binaries
+    { sleep 12; echo "root"; sleep 1; echo "./gdbserver 0.0.0.0:8002 $COMMAND" ; echo "poweroff" ; } | arm_now start mips32 --clean --sync --redir tcp:8001::8002 > /dev/null
+}
+
+( start_emul ) &
+sleep 14
+
+case "$1" in
+    "trace")
+        gdb-multiarch \
+            -ex "target remote localhost:8001" \
+            -ex "set heuristic-fence-post 0" \
+            -ex "set confirm off" \
+            -ex "generate-core-file" \
+            -x "./gdb_trace.py"
+        mv core.* coredump
+        ;;
+    *)
+        gdb-multiarch \
+            -ex "target remote localhost:8001" \
+            -ex "layout regs"
+        ;;
+esac
+
+# killall qemu-system-mips
